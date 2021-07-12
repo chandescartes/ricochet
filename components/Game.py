30a281f4351931eb2be85d9c22959062c9b059ee
Chanyoung Ryu <chandescartes@gmail.com>
2021-07-13 01:33:44 +0900

    Implement basic game

---
+++ components/Game.py
@@ -0,0 +1,21 @@
+
+from components.Target import Target
+from components.Robot import Robot
+
+class Game:
+    def __init__(self, board, num_robots=5):
+        self.board = board
+        self.num_robots = num_robots
+        
+        self.robots = {}
+        for i in range(self.num_robots):
+            robot_name = chr(65 + i)
+            self.robots[robot_name] = Robot(robot_name)
+    
+    def set_target(self, robot, row, col):
+        if self.target:
+            row, col = self.target.row, self.target.col
+            tile = self.board.get_tile(row, col)
+            tile.clear_target()
+
+        self.target = Target(robot.name, row, col)
